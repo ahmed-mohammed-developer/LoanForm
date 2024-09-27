93be9ee809438a00ca331165db96413808fc944e
ahmed <ahmed.zaakan@gmail.com>
2024-09-27 18:01:51 +0300

    first commit

--- src/LoanForm.js
+++ src/LoanForm.js
@@ -2,7 +2,22 @@ import './LoanForm.css'
 import Modal from './Modal';
 import { useState } from 'react';
 
+
+
+
+
+
 export default function LoanForm() {
+    const [loanInputs, setLoanInputs] = useState({
+        name: '',
+        phoneNumber: '',
+        age: '',
+        isEmplye: false,
+        salery: "",
+    })
+
+
+
     return(
         <div className='LoanForm'>
             <form className='forms'>
@@ -10,28 +25,56 @@ export default function LoanForm() {
                 <hr></hr>
 
                 <label>Name</label>
-                <input />
+                <input 
+                value={loanInputs.name}
+                onChange={(event) => {
+                    setLoanInputs({...loanInputs, name: event.target.value})
+                }}
+                />
 
 
                 <label>Phone Number</label>
-                <input />
+                <input
+                  value={loanInputs.phoneNumber}
+                  onChange={(event) => {
+                      setLoanInputs({...loanInputs, phoneNumber: event.target.value})
+                  }}
+                />
 
 
                 <label>Age</label>
-                <input />
+                <input
+                  value={loanInputs.age}
+                  onChange={(event) => {
+                      setLoanInputs({...loanInputs, age: event.target.value})
+                  }}
+                />
 
 
                 <label>Are you an employee</label>
-                <input type='checkbox' />
+                <input type='checkbox' checked={loanInputs.isEmplye}
+                onChange={(event) => {
+                    setLoanInputs({...loanInputs, isEmplye: event.target.checked})
+                }}
+                />
 
                 <label>Salery</label>
-                <select>
+                <select
+                  value={loanInputs.salery}
+                  onChange={(event) => {
+                      setLoanInputs({...loanInputs, salery: event.target.value})
+                  }}
+                >
                     <option>less then 500$</option>
                     <option>between 500$ and 2000$ </option>
                     <option>above 2000$</option>
                 </select>
 
-                <button>Submit</button>
+                <button
+                className=''
+                onClick={handleFormSubmit}
+                disabled={btnIsDisabled}
+                >Submit</button>
 
             </form>
             {/*<Modal />*/}
